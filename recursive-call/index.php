97c6dcf52e95d620a8311c7ed8cec95b83252333
nacho <nacho.albiol88@gmail.com>
2024-09-27 17:32:24 +0200

    Correct names for input & functions kata recursive-call

--- recursive-call/index.php
+++ recursive-call/index.php
@@ -1,30 +1,30 @@
 <?php
 
-$intput1 = 5;
-$intput2 = 3;
+$input1 = 5;
+$input2 = 3;
 
 // Recursive function that prints numbers from the input to 0.
-function printNumbers(int $input): void
+function countDown(int $input): void
 {
     if ($input > 0) {
         echo $input . ",";
-        printNumbers($input - 1);
+        countDown($input - 1);
     } else {
         // PHP_EOL is a constant that represents the end of line character "/n".
         echo $input . PHP_EOL;
     }
 }
 
-printNumbers($intput1);
-printNumbers($intput2);
+countDown($input1);
+countDown($input2);
 
 
 // Recursive function that prints factorial of a number.
-function calculateFactorial(int $input): int
+function getFactorial(int $input): int
 {
     if ($input > 0) {
         $newInput = $input - 1;
-        $factorial = $input * calculateFactorial($newInput);
+        $factorial = $input * getFactorial($newInput);
         return $factorial;
     } else {
         // The factorial of 0 is 1.
@@ -32,8 +32,23 @@ function calculateFactorial(int $input): int
     }
 }
 
-echo "El factorial de {$intput1} es: " . calculateFactorial($intput1) . PHP_EOL;
-echo "El factorial de {$intput2} es: " . calculateFactorial($intput2) . PHP_EOL;
+echo "El factorial de {$input1} es: " . getFactorial($input1) . PHP_EOL;
+echo "El factorial de {$input2} es: " . getFactorial($input2) . PHP_EOL;
+
+// Recursive function that prints Fibonacci series until the given number.
+// function fibonacci(int $input): int
+// {
+//     if ($input <= 1) {
+//         return fibonacci($input - 1) + fibonacci($input - 2);
+
+//     } else {
+//         return $input;
+//     }
+// }
+
+
+// echo "La serie de Fibonacci hasta el elemento Nº{$input1} es: " . fibonacci($input1) . PHP_EOL;
+// echo "La serie de Fibonacci hasta el elemento Nº{$input2} es: " . PHP_EOL;
 
 /*
 
