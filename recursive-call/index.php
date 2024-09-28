80319691a9dbbe81d588ea3e2d3f250e38d75086
nacho <nacho.albiol88@gmail.com>
2024-09-28 11:43:18 +0200

    add Fibonacci function to kata recursive-call

--- recursive-call/index.php
+++ recursive-call/index.php
@@ -35,24 +35,22 @@ function getFactorial(int $input): int
 echo "El factorial de {$input1} es: " . getFactorial($input1) . PHP_EOL;
 echo "El factorial de {$input2} es: " . getFactorial($input2) . PHP_EOL;
 
-// Recursive function that prints Fibonacci series until the given number.
-// function fibonacci(int $input): int
-// {
-//     if ($input <= 1) {
-//         return fibonacci($input - 1) + fibonacci($input - 2);
+// NEED TO CHECK THIS ONE, IT'S NOT WHAT I WANT.
+// Recursive function that returns the Fibonacci series up to the given index.
+function fibonacci(int $index, array $fibonacciNumbers = [0, 1]): array
+{
+    // Base case: Return the array when it has $index elements.
+    if (count($fibonacciNumbers) >= $index) {
+        return $fibonacciNumbers;
+    }
+    
+    // Recursive case: Add the next Fibonacci number.
+    $fibonacciNumbers[] = $fibonacciNumbers[count($fibonacciNumbers) - 1] + $fibonacciNumbers[count($fibonacciNumbers) - 2];
+    
+    // Recursive call.
+    return fibonacci($index, $fibonacciNumbers);
+}
 
-//     } else {
-//         return $input;
-//     }
-// }
 
-
-// echo "La serie de Fibonacci hasta el elemento Nº{$input1} es: " . fibonacci($input1) . PHP_EOL;
-// echo "La serie de Fibonacci hasta el elemento Nº{$input2} es: " . PHP_EOL;
-
-/*
-
-Bonus track:
-
-Per últim, mostra la serie de Fibonacci d'un nombre donat.
-*/
+echo "La serie de Fibonacci hasta el elemento Nº{$input1} es: " . implode(", ", fibonacci($input1)) . PHP_EOL;
+echo "La serie de Fibonacci hasta el elemento Nº{$input2} es: " . implode(", ", fibonacci($input2)) . PHP_EOL;
