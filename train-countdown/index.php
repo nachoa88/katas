ae4c99eb2351b5c8bc79e3ea14c14aea1290da2b
nacho <nacho.albiol88@gmail.com>
2024-12-28 11:45:55 +0100

    Add input validation & changed switch for if statement

--- train-countdown/index.php
+++ train-countdown/index.php
@@ -1,25 +1,49 @@
 <?php
 
-$limit = 67;
-$sum = 0;
-$maxInputs = 10;
-$currentInput = 0;
-echo "You are in a train and the brakes are not working, you have enter reach 67 in 10 tries to make the train stop.\n";
-// input to enter a number between 1 and 9
-while ($currentInput < $maxInputs) {
-    $input = readline("Enter a number between 1 and 9: ");
-    $sum += $input;
-    $currentInput++;
+const MIN_INPUT = 1;
+const MAX_INPUT = 9;
 
-    switch ($sum) {
-        case $sum < $limit:
-            echo "The sum is: " . $sum . " and is your " . $currentInput . " try.\n";
-            break;
-        case $limit:
-            echo "You've saved your life, the train brakes!!.\n";
-            break 2;
-        default:
-            echo "The brake is not working!! Say goodbye to this world!!.\n";
-            break 2;
+// Validate input
+function getValidatedInput(): int 
+{
+    while (true) {
+        $input = readline("Enter a number between 1 and 9: ");
+        if (isValidInput($input)) {
+            return (int) $input;
+        }
+        echo "Invalid input! Use numbers between 1 and 9.\n";
     }
 }
+
+function isValidInput(mixed $input): bool 
+{
+    return is_numeric($input) 
+        && (int) $input >= MIN_INPUT 
+        && (int) $input <= MAX_INPUT;
+}
+
+
+echo "Train brakes failed! Reach exactly 67 in 10 tries to activate the emergency brake!\n";
+function activateEmergencyBrake(int $maxInputs, int $limit): void
+{
+    $currentInput = 0;
+    $sum = 0;
+
+    while ($currentInput < $maxInputs) {
+        $input = getValidatedInput();
+        $sum += $input;
+        $currentInput++;
+        
+        if ($sum < $limit && $currentInput < $maxInputs) {
+            echo "The sum is: " . $sum . " and is your " . $currentInput . " try.\n";
+        } elseif ($sum === $limit) {
+            echo "You've saved your life, the train is stopping!!.\n";
+            break;
+        } else {
+            echo "Catastrophic failure! Brake is not responding!\n";
+            break;
+        }
+    }
+}
+
+activateEmergencyBrake(10, 67);
